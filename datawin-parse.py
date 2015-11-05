6715101698b0cc848884564a2560e3ebd3959003
Thomas Schneider <maxmusterm@gmail.com>
2015-11-05 18:34:54 +0100

    So I can extract chunks.But I can't really parse them (so text is 00 xx 00 00 00 where xx is the text size [probably 00 00 00 is part of it as well but hmm] but the parts before that I don't get

--- datawin-parse.py
+++ datawin-parse.py
@@ -2,12 +2,17 @@
 import os,sys
 import binascii
 import struct
+import StringIO
 
 # chunk names are 4 bytes long
 # not quite sure how long the header is but I guess 8 bytes for now :>
 dwheader=binascii.unhexlify("464F524DE6EF5A03")
 datawin=open("data.win","r+")
+datawin.seek(0,2)
+dwsize=datawin.tell()
+datawin.seek(0,0)
 header=datawin.read(8)
+chunks={}
 
 if header==dwheader:
     print "Doogie doogie Dodger"
@@ -19,7 +24,6 @@ else:
 def read_chunk():
     chunkname=datawin.read(4)
     print "Reading "+chunkname
-    print datawin.tell()
     chunksize=datawin.read(4)
     # TODO: _THIS_ is stupid! ... let's correct this with a nice struct or somethin
     # later...
@@ -27,32 +31,9 @@ def read_chunk():
     chunksize=foo[-2:]+foo[4:6]+foo[2:4]+foo[:2]
     chunksize=int(chunksize,16)
     chunk=datawin.read(chunksize)
+    return chunkname, chunk
 
 
-print datawin.tell()
-read_chunk()
-read_chunk()
-read_chunk()
-read_chunk()
-read_chunk()
-read_chunk()
-read_chunk()
-read_chunk()
-read_chunk()
-read_chunk()
-read_chunk()
-read_chunk()
-read_chunk()
-read_chunk()
-read_chunk()
-read_chunk()
-read_chunk()
-read_chunk()
-read_chunk()
-read_chunk()
-read_chunk()
-read_chunk()
-read_chunk()
-read_chunk()
-read_chunk()
-
+while dwsize!=datawin.tell():
+    chunk=read_chunk()
+    chunks[chunk[0]]=chunk[1]
